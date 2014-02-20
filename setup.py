39031f012078cad7534a120957171e2746c7b2bb
Mike Bryant <mike@mikebryant.me.uk>
2014-02-20 01:25:47 +0000

    Add setup.py, and basic structure.

---
+++ setup.py
@@ -0,0 +1,26 @@
+'''Setup file for django-autoconfig.'''
+from setuptools import setup
+from django_autoconfig.version import __VERSION__
+
+setup(
+    name='django-autoconfig',
+    version=__VERSION__,
+    packages=['django_autoconfig'],
+    description='Automatic configuration of Django projects based on the application requirements.',
+    author='Mike Bryant',
+    author_email='mike@mikebryant.me.uk',
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Framework :: Django',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+    ],
+)
