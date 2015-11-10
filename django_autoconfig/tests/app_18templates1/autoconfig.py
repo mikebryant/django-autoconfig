591f00c771e282e96f19ca6f0527f46ca3bbb2fb
Mike Bryant <mike@mikebryant.me.uk>
2015-11-10 01:29:16 +0000

    Special case the TEMPLATES setting, fixes #19

---
+++ django_autoconfig/tests/app_18templates1/autoconfig.py
@@ -0,0 +1,13 @@
+SETTINGS = {
+    'TEMPLATES': [
+        {
+            'BACKEND': 'django.template.backends.django.DjangoTemplates',
+            'APP_DIRS': True,
+            'OPTIONS': {
+                'context_processors': [
+                    'django.template.context_processors.request',
+                ],
+            },
+        },
+    ],
+}
---
+++ django_autoconfig/tests/app_18templates2/autoconfig.py
@@ -0,0 +1,12 @@
+SETTINGS = {
+    'TEMPLATES': [
+        {
+            'BACKEND': 'django.template.backends.django.DjangoTemplates',
+            'OPTIONS': {
+                'context_processors': [
+                    'context.processor.2',
+                ],
+            },
+        },
+    ],
+}
